02550e2bb8085fb4567f9d384c7342bebbbc8c09
Ekroop05 <ekroopsingh2005@gmail.com>
2026-03-04 23:08:10 +0530

    added GET products route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,10 +10,10 @@ import {
 
 const router = express.Router();
 
-router.post("/product", createProduct);          // CREATE
-router.get("/products", getProducts);            // READ ALL
-router.get("/product/:id", getProductById);      // READ ONE
-router.put("/product/:id", updateProduct);       // UPDATE
-router.delete("/product/:id", deleteProduct);    // DELETE
+router.post("/product", createProduct);
+router.get("/products", getProducts);
+router.get("/product/:id", getProductById);
+router.put("/product/:id", updateProduct);
+router.delete("/product/:id", deleteProduct);
 
 export default router;
